f49644a3507ba5e662df43a5bef1e3b619613a4c
Sophie Strausberg <sophie.strausberg@gmail.com>
2022-01-15 16:49:18 -0500

    Initial Commit

---
+++ AppStoreClone/AppStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneApp.swift
+//  AppStoreClone
+//
+//  Created by Sophie Strausberg on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStoreClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppStoreClone
+//
+//  Created by Sophie Strausberg on 1/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
